3d62c38fa9d7d3e21bdc124247bc060e0de92941
eduardo <eduardomendes418@gmail.com>
2026-02-07 16:20:46 -0300

    [ERP-87] 🔧 fix - useAdminAuthStore

--- src/stores/adminAuthStore.ts
+++ src/stores/adminAuthStore.ts
@@ -1,21 +1,24 @@
-import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface AdminAuthState {
-  isAdminAuthenticated: boolean;
-  adminLogin: () => void;
-  adminLogout: () => void;
+	isAdminAuthenticated: boolean;
+	adminLogin: () => void;
+	adminLogout: () => void;
 }
 
 export const useAdminAuthStore = create<AdminAuthState>()(
-  persist(
-    (set) => ({
-      isAdminAuthenticated: false,
-      adminLogin: () => set({ isAdminAuthenticated: true }),
-      adminLogout: () => set({ isAdminAuthenticated: false }),
-    }),
-    {
-      name: 'admin-auth-storage', // name of the item in the storage (must be unique)
-    }
-  )
+	persist(
+		(set) => ({
+			isAdminAuthenticated: false,
+			adminLogin: () => set({ isAdminAuthenticated: true }),
+			adminLogout: () => {
+				set({ isAdminAuthenticated: false });
+				window.localStorage.removeItem("admin-auth-storage");
+			},
+		}),
+		{
+			name: "admin-auth-storage", // name of the item in the storage (must be unique)
+		},
+	),
 );
